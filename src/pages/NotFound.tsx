d34419319fb5874d056cf681bf98c81b3f0b8a91
Port-Arthur-Police-Department <ryan.byers@portarthurtx.gov>
2025-11-13 14:04:23 -0600

    Update NotFound.tsx

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,13 +1,27 @@
-// pages/NotFound.tsx
 import { useLocation, Link } from "react-router-dom";
+import { useEffect } from "react";
 
 const NotFound = () => {
   const location = useLocation();
-  
+
+  useEffect(() => {
+    console.error('404 - Route not found:', location.pathname);
+    
+    // If we hit a 404, try to redirect to home
+    if (location.pathname !== '/') {
+      console.log('Attempting to redirect to home...');
+      // Use setTimeout to avoid React state update during render
+      setTimeout(() => {
+        window.location.hash = '/';
+      }, 100);
+    }
+  }, [location]);
+
   return (
     <div style={{ padding: '2rem', textAlign: 'center' }}>
       <h1>404 - Page Not Found</h1>
       <p>The page <code>{location.pathname}</code> does not exist.</p>
+      <p>Current hash: <code>{window.location.hash}</code></p>
       <Link to="/">Return to Home</Link>
     </div>
   );
