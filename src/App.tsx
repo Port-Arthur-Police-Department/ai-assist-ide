56e25fb4b1632a249e87d052bdd8fe7496364cb6
Port-Arthur-Police-Department <ryan.byers@portarthurtx.gov>
2025-11-13 15:05:07 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,11 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter, Routes, Route, useLocation } from "react-router-dom";
-import { useEffect } from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
-
-// Debug component to log routing
-const RouteDebugger = () => {
-  const location = useLocation();
-  
-  useEffect(() => {
-    console.log('Current route:', location.pathname + location.search);
-    console.log('Full URL:', window.location.href);
-  }, [location]);
-
-  return null;
-};
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
@@ -27,10 +14,8 @@ const App = () => (
       <Toaster />
       <Sonner />
       <HashRouter>
-        <RouteDebugger />
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </HashRouter>
