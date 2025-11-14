913fdea0085f07cf25103a4b52821b3cd088e2a0
Port-Arthur-Police-Department <ryan.byers@portarthurtx.gov>
2025-11-13 18:06:23 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,16 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Clear any old service workers and register new one
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(registrations => {
+    registrations.forEach(registration => {
+      console.log('Unregistering old service worker:', registration.scope);
+      registration.unregister();
+    });
+    
+    // After clearing, the new service worker will auto-register via VitePWA
+  });
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
